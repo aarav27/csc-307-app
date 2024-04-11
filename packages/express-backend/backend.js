635f24214aeeb1e88d2a7c535355cda54d4a7c9c
aarav27 <aaravsharma927@gmail.com>
2024-04-11 11:45:21 -0700

    Getting users by Id and refactored code

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -4,16 +4,6 @@ import express from "express";
 const app = express();
 const port = 8000;
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.send("Hello World!")
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
-
 const users = {
     users_list: [
       {
@@ -44,13 +34,25 @@ const users = {
     ]
   };
 
-  const findUserByName = (name) => {
+const findUserByName = (name) => {
     return users["users_list"].filter(
         (user) => user["name"] === name
     );
   }
 
-  app.get('/users', (req,res) => {
+const findUserById = (id) => {
+    return users["users_list"].find(
+        (user) => user["id"] === id
+    )
+  }
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+});
+
+app.get('/users', (req,res) => {
     const name = req.query.name;
     if(name != undefined){
         let result = findUserByName(name);
@@ -60,4 +62,20 @@ const users = {
     else{
         res.send(users)
     }
-  });
+});
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params["id"]
+    let result = findUserById(id);
+    console.log(result)
+    if(result === undefined){
+        res.status(404).send("Resource Not Found");
+    }
+    else{
+        res.send(result);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
