779f3f709a564a97108665a826f538ac485754fd
aarav27 <aaravsharma927@gmail.com>
2024-04-11 12:48:06 -0700

    1.Adding User Using POST 2.Delete User By ID Using DELETE 3.Get User Given Name And Job

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -46,6 +46,25 @@ const findUserById = (id) => {
     )
   }
 
+const findUserByNameJob = (name, job) => {
+    return users["users_list"].filter(
+        (user) => (user["name"] === name && user["job"] === job)
+    )
+}
+
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user
+}
+
+const deleteUserByID = (id) => {
+    const userToDelete = users["users_list"].find(
+        (user) => user["id"] == id
+    )
+    users["users_list"].pop(userToDelete);
+    return userToDelete;
+}
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -54,8 +73,12 @@ app.get("/", (req, res) => {
 
 app.get('/users', (req,res) => {
     const name = req.query.name;
+    const job = req.query.job
     if(name != undefined){
         let result = findUserByName(name);
+        if (job != undefined){
+            result = findUserByNameJob(name, job)
+        }
         result = { users_list: result };
         res.send(result)
     }
@@ -75,6 +98,18 @@ app.get('/users/:id', (req, res) => {
         res.send(result);
     }
 });
+
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send(`Added User With User ID ${userToAdd["id"]}`);
+})
+
+app.delete('/users', (req, res) => {
+    const user_id = req.body
+    deleteUserByID(user_id["id"]);
+    res.send(`Deleted User With User ID ${user_id["id"]}`);
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
